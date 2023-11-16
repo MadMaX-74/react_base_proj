2ee2e6c857390f0d7c3914671b151b5559192209
MaxWebDev <maximus3506@mail.ru>
2023-11-16 23:58:07 +0300

    add store

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,7 +51,8 @@ module.exports = {
         ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
         "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "error"
+        "react-hooks/exhaustive-deps": "error",
+        "no-param-reassign": off
     },
     globals: {
         __IS_DEV__: true,
